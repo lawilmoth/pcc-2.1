472d000525be6f46ee062449df4da0a66d47068a
= <=>
2024-08-28 22:06:21 -0400

    assigment 2.1

--- test_assignment.py
+++ test_assignment.py
@@ -1,5 +1,4 @@
 import pytest
-import numpy as np
 import  assignment
 
 def test_hello():
@@ -10,6 +9,21 @@ def test_hello():
 def test_add_two(input, expected):
     assert assignment.add_two(input) == expected
 
+def test_convert_to_lowercase():
+    assert assignment.convert_to_lowercase("HELLO") == "hello"
 
-def test_zero_array():
-    return np.testing.assert_allclose(assignment.zero_array(100), np.zeros(shape=(100,)))
+def test_convert_to_uppercase():    
+    assert assignment.convert_to_uppercase("hello") == "HELLO"
+
+def test_convert_to_titlecase():
+    assert assignment.convert_to_titlecase("hello") == "Hello"
+
+def test_add_exclamation_mark():
+    assert assignment.add_exclamation_mark("hello") == "hello!"
+
+def test_subtract_three_point_five():
+    assert assignment.subtract_three_point_five(10) == 6.5
+
+def test_multiply_by_five():
+    assert assignment.multiply_by_five(10) == 50
+
